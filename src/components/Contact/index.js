3449b728948ebe55c477b2d0628414309713857f
Patrick Sheng <sheng.patrick@gmail.com>
2025-11-12 17:46:58 +1300

    feat: added a contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,99 @@
-const Contact = () => {
+import { useEffect, useState, useRef } from 'react';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import emailjs from '@emailjs/browser';
+import './index.scss';
 
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const form = useRef();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        console.log('Service ID:', process.env.REACT_APP_EMAILJS_SERVICE_ID);
+        console.log('Template ID:', process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
+        console.log('Public Key:', process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
+
+        emailjs
+            .sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID,
+                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+                form.current,
+                process.env.REACT_APP_EMAILJS_USER_ID)
+            .then(
+                () => {
+                    alert('Message successfully sent!');
+                    window.location.reload(false);
+                },
+                () => {
+                    alert('Failed to send the message, please try again');
+                }
+            );
+    };
+
+    return (
+        <>
+            <div className="container contact-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+                    </h1>
+                    <p>
+                        I'm interested in freelance opportunities and full-time positions.
+                        If you have any requests or questions, don't hesitate to contact me
+                        using the form below.
+                    </p>
+                    <div className="contact-form">
+                        <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input
+                                        placeholder="Email"
+                                        type="email"
+                                        name="email"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <input
+                                        placeholder="Subject"
+                                        type="text"
+                                        name="subject"
+                                        required
+                                    />
+                                </li>
+                                <li>
+                                    <textarea
+                                        placeholder="Message"
+                                        name="message"
+                                        required
+                                    ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
 };
 
 export default Contact;
