976f326b2768411805900f30a6df4196b5ebada1
Patrick Sheng <sheng.patrick@gmail.com>
2025-11-12 17:47:30 +1300

    fix: removed debugging lines

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -18,10 +18,6 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        console.log('Service ID:', process.env.REACT_APP_EMAILJS_SERVICE_ID);
-        console.log('Template ID:', process.env.REACT_APP_EMAILJS_TEMPLATE_ID);
-        console.log('Public Key:', process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
-
         emailjs
             .sendForm(process.env.REACT_APP_EMAILJS_SERVICE_ID,
                 process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
